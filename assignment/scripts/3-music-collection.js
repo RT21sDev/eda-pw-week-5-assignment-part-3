e72e01ba050246bbb3ea50c6cad3f1c6d6838fd4
RT21sDev <171520861+RT21sDev@users.noreply.github.com>
2024-08-18 14:00:49 -0500

    Update 3-music-collection.js mk1

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,13 +1,52 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
+let myCollection = [];
 
+function addToCollection(collection, title, artist, yearPublished) {
 
+  let album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  };
+  
+  collection.push(album);
+  
+  return album;
+}
+console.log(addToCollection(myCollection, 'Abbey Road', 'The Beatles', 1969));
+console.log(addToCollection(myCollection, 'Thriller', 'Michael Jackson', 1982));
+console.log(addToCollection(myCollection, 'Back in Black', 'AC/DC', 1980));
+console.log(addToCollection(myCollection, 'Nevermind', 'Nirvana', 1991));
+console.log(addToCollection(myCollection, 'The Dark Side of the Moon', 'Pink Floyd', 1973));
+console.log(addToCollection(myCollection, '21', 'Adele', 2011));
 
+console.log(myCollection);
 
+function showCollection(collection) {
+  collection.forEach(album => {
+    console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`);
+  });
+}
 
+showCollection(myCollection);
 
+function findByArtist(collection, artist) {
 
+  let results = [];
+  
+  collection.forEach(album => {
+    if (album.artist === artist) {
+      results.push(album);
+    }
+  });
+  
+  return results;
+}
 
+console.log(findByArtist(myCollection, 'The Beatles')); 
+console.log(findByArtist(myCollection, 'Nirvana')); 
+console.log(findByArtist(myCollection, 'U2')); 
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
 // as a lil' chunk of friendly code that you don't need to understand right now.
